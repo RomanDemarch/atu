6142d85da53a58eadb010eb32ce801f9086f79d9
RomanDemarch <90706192+RomanDemarch@users.noreply.github.com>
2025-05-11 19:57:29 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -148,7 +148,6 @@ const handleCommuneClick = (feature, layer) => {
         });
       }
     });
-  };
 
   const filterCommunes = (communes, parentId) =>
     communes.features.filter((c) => c.properties.PARENT === parentId);
@@ -158,7 +157,6 @@ const handleCommuneClick = (feature, layer) => {
     [57.0, 36.0],
   ];
 
-function ModelMap() {
   return (
     <div className="h-screen w-full">
       <MapContainer center={[53.7, 27.9]} zoom={7} minZoom={6} maxBounds={maxBounds} maxBoundsViscosity={1.0} className="h-full w-full z-0"
